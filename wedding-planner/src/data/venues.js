8e11fa32d141362e247b4d16e75f790539a98708
Apurva Salphale <salphaleapurvaa@gmail.com>
2025-08-24 14:00:05 +0530

    data/venues.js added

---
+++ wedding-planner/src/data/venues.js
@@ -0,0 +1,168 @@
+
+
+const venues = [
+  {  id: 1,
+    name: "Royal Palace",
+    location: "Jaipur",
+    type: "Destination Wedding",
+    rating: 4.7,
+    reviews: 120,
+    price: 2500,
+    guests: 300,
+    rooms: 25,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D1.jpeg"
+  },
+  {
+    id:2,
+    name: "Sunset Banquet",
+    location: "Delhi",
+    type: "Banquet Hall",
+    rating: 4.3,
+    reviews: 95,
+    price: 1800,
+    guests: 200,
+    rooms: 15,
+    vegNonveg: "Veg Only",
+    image: "/images/D2.jpeg"
+  },
+  {
+    id:3,
+    name: "The Hilltop Resort",
+    location: "Udaipur",
+    type: "Destination Wedding",
+    rating: 4.9,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D3.jpeg"
+  },
+
+  {
+    id:4,
+    name: "The Sky  Resort",
+    location: "Udaipur",
+    type: "Banquet Hall",
+    rating: 4.4,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D1.jpeg"
+  },
+
+  {
+    id:5,
+    name: "The GreenHill Resort",
+    location: "Mumbai",
+    type: "Resort",
+    rating: 4,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D4.jpeg"
+  },
+
+  {
+    id:6,
+    name: "The Sandalwood Resort",
+    location: "Udaipur",
+    type: "Resort",
+    rating: 4.2,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D1.jpeg"
+  },
+
+  {
+    id:7,
+    name: "The Hilltop Resort",
+    location: "Udaipur",
+    type: "Lawn",
+    rating: 4.9,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D2.jpeg"
+  },
+
+  {
+    id:8,
+    name: "Farmhouse",
+    location: "Nashik",
+    type: "Farmhouse",
+    rating: 4.9,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D3.jpeg"
+  },
+
+  {
+    id:9,
+    name: "Stay",
+    location: "Karad",
+    type: "Farmhouse",
+    rating: 4.3,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D1.jpeg"
+  },
+
+  {
+    id:10,
+    name: "Grand Ratanlal",
+    location: "Pune",
+    type: "Lawn",
+    rating: 4.9,
+    reviews: 180,
+    price: 3200,
+    guests: 350,
+    rooms: 40,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D3.jpeg"
+  },
+
+   {  id: 11,
+    name: "Royal Palace",
+    location: "Jaipur",
+    type: "Destination Wedding",
+    rating: 4.7,
+    reviews: 120,
+    price: 2500,
+    guests: 300,
+    rooms: 25,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D1.jpeg"
+  },
+  {  id: 12,
+    name: "Grand Palace",
+    location: "Nashik",
+    type: "Destination Wedding",
+    rating: 4.7,
+    reviews: 120,
+    price: 2500,
+    guests: 300,
+    rooms: 25,
+    vegNonveg: "Veg & Non-Veg",
+    image: "/images/D2.jpeg"
+  },
+
+  // Add more venue objects as needed
+];
+export default venues;
